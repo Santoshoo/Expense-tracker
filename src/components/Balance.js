222fce4b48a133a3938c48568c8507c89cfd69ba
Santosh <sahosantosh117@gmail.com>
2024-07-07 20:21:23 +0530

    first commit

---
+++ src/components/Balance.js
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
+
+ const Balance = () => {
+    const { transactions } = useContext(GlobalContext);
+    var total = 0;
+    transactions.map(transaction => total += transaction.amount)
+    const sign = total < 0 ? '-' : '+';
+    return (
+        <>
+            <h4>Your Balance</h4>
+            <h1>{sign}${Math.abs(total)} </h1>
+        </>
+    )
+}
+export default Balance;
